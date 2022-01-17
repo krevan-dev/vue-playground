4c2a026ab6b1f33bf5061ffb662d52f3562f9d7a
Kyle Evans <krevan88@protonmail.com>
2022-01-17 15:48:38 -0700

    Completed sections

--- src/components/EventsExercise.vue
+++ src/components/EventsExercise.vue
@@ -12,8 +12,8 @@
             property to display its value.
           </p>
           <!-- add event listener to the button element -->
-          <button class="btn btn-primary m-2">Add 1</button>
-          <p>The button has been clicked {{}} times.</p>
+          <button @click="state.counter++" class="btn btn-primary m-2">Add 1</button>
+          <p>The button has been clicked {{state.counter}} times.</p>
         </div>
         <div class="border p-1">
           <h5>Events Calling A Method</h5>
@@ -26,7 +26,7 @@
             "greeting" property and pass its value into an alert().
           </p>
           <!-- add event listener to the button element -->
-          <button class="btn btn-primary m-2">Greet</button>
+          <button class="btn btn-primary m-2" @click="greet">Greet</button>
         </div>
       </div>
     </div>
@@ -40,13 +40,15 @@ export default {
   name: "events-exercise",
   setup() {
     const state = reactive({
-      //add property called "counter"
-      //add property called "greeting"
+      counter: 0,
+      greeting: "Hello world!"
       //create a method called "greet"
     });
     return {
       state,
-      // Add your methods here
+      greet(){
+        alert(state.greeting)
+      }
     };
   },
   components: {},
